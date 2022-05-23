8856cd049468fd0907f28dc9bd725b20ce67dc24
SlavaKlepikov <vjacheslavmailk@gmail.com>
2022-05-23 12:34:20 +0300

    add scripts for lessons 8

---
+++ custom_cache.py
@@ -0,0 +1,33 @@
+import time
+
+
+def decorator(func):
+    result = {}
+
+    def wrapper(n):
+        if not result.get(n):
+            result[n] = func(n)
+            return result.get(n)
+        else:
+            return result.get(n)
+
+    return wrapper
+
+
+@decorator
+def main(i):
+    time.sleep(i / 10)
+    return i ** 2
+
+
+if __name__ == "__main__":
+    start = time.time()
+    results = []
+    values = (1, 2, 3, 2, 3, 1, 2, 2, 3, 4, 1, 2, 3, 2, 1, 4, 4, 1, 2, 4)
+    print(values)
+    for i in values:
+        results.append(main(i))
+    print(results)
+    end = time.time()
+    print(f"Total time: {end - start}")
+
